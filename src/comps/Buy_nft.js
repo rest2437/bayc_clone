a718c21c6ebb1b8778482494419766f54710b354
rest2437 <nocturnalrd@gmail.com>
2022-01-07 15:02:52 -0800

    setup complete

---
+++ src/comps/Buy_nft.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Buy_nft() {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default Buy_nft;
---
+++ src/pages/members/Apes_vs_mutants.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Apes_vs_mutants() {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default Apes_vs_mutants;
