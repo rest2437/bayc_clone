73447f0d8ebad85882234fd32bef2b65a973188e
rest2437 <nocturnalrd@gmail.com>
2022-01-25 19:35:36 -0800

    styling landing page

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div>
+    <div className="navbar">
       <h1>Navbar</h1>
     </div>
   );
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,10 +3,30 @@ import { Link } from "react-router-dom";
 
 function Landing() {
   return (
-    <div>
-      <h1>hello this is landing</h1>
+    <div
+      className="container"
+      style={{ backgroundColor: "black", height: "700px", width: "100%" }}
+    >
+      <h1 style={{ color: "white" }}>
+        Welcome to the <br /> Blah Blah Blah
+      </h1>
       <Link to="/Home">
-        <button>ENTER</button>
+        <button
+          style={{
+            color: "black",
+            fontSize: "16px",
+            fontWeight: "bold",
+            backgroundColor: "#CEFA05",
+            width: "300px",
+            height: "50px",
+            borderRadius: "10px",
+            border: "0px",
+            cursor: "pointer",
+            marginTop: "30rem",
+          }}
+        >
+          ENTER
+        </button>
       </Link>
     </div>
   );
